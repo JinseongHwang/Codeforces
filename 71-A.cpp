b69465418159ee6647d89b423d17f498ee4caa40
JinseongHwang <eddy5360@naver.com>
2021-01-30 22:47:37 +0900

    A. Way Too Long Words

--- 71-A.cpp
+++ 71-A.cpp
@@ -6,7 +6,7 @@ int main() {
 	int n; cin >> n;
 	while (n--) {
 		string input; cin >> input;
-		if (input.length() > 10) {
+		if (input.length() > 10) { // 10보다 길다는 조건에 만족하면 아래와 같이 문자열을 가공한다.
 			input = input.front() + to_string(input.length() - 2) + input.back();
 		}
 		cout << input << '\n';
